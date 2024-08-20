0c273dc28009049e927a70e4a7375db4042eb8a4
Pont-SimoSiren <sirensimo7@gmail.com>
2024-08-20 12:54:22 +0300

    theme toimii alustavasti

---
+++ theme.js
@@ -0,0 +1,16 @@
+
+function dark() {
+    let page = document.getElementById("page")
+
+    page.style.backgroundColor = "black";
+    page.style.color = "silver";
+
+}
+
+function light() {
+    let page = document.getElementById("page")
+
+    page.style.backgroundColor = "white";
+    page.style.color = "black";
+
+}
