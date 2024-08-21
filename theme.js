fe37c219f9d35b6e80245aa97265d346d71a6710
Pont-SimoSiren <sirensimo7@gmail.com>
2024-08-21 09:22:54 +0300

    teema juttu valmis

--- theme.js
+++ theme.js
@@ -1,11 +1,24 @@
 
+// Alussa tarkistetaan onko väriteemaa talletettu
+
+let savedTheme = localStorage.getItem("savedTheme")
+
+if (savedTheme == "dark") {
+    dark()
+}
+
+
+//---------Väriteeman muuttamisfunktiot------------------------------
+
 function dark() {
     let page = document.getElementById("page")
 
     page.style.backgroundColor = "black";
     page.style.color = "silver";
 
+    localStorage.setItem("savedTheme", "dark")
 }
+
 
 function light() {
     let page = document.getElementById("page")
@@ -13,4 +26,5 @@ function light() {
     page.style.backgroundColor = "white";
     page.style.color = "black";
 
+    localStorage.setItem("savedTheme", "light")
 }
